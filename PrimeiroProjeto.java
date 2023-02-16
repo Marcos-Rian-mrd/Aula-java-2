351eb78de7392f8c7c30da14b5d5220aecbf1fd7
marcos <marcosrianmrd@gmail.com>
2023-02-16 17:22:36 -0300

    aula2

---
+++ PrimeiroProjeto.java
@@ -0,0 +1,14 @@
+    public class PrimeiroProjeto {
+        public static void main(String[] args){
+            // armazenar propriedades e variaveis
+            System.out.println(2+2);
+            System.out.println(3+3);
+            System.out.println("ola texto");
+
+            // comentarios // => sao ulteis para indicar comentarios
+            // ponto de entrada - entrey-point --> é o ponto onde todos sistemas procuram no seu projeto.
+            // metodos: sao intrucoes que defini para o progama executar . exemplo:
+            // intrução chamada System.out.println("")
+            // propriedades/variaveis --> será onde iremos armazenar dados para manipularmos no futuro.
+        }
+    }
